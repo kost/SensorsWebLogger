6fa766a61cff934af049beb31e5c1d5a86b370a4
Vlatko Kosturjak <kost@linux.hr>
2015-01-14 07:03:40 +0100

    Log is not used

--- app/src/main/java/org/kost/android/sensorsweblogger/MainActivity.java
+++ app/src/main/java/org/kost/android/sensorsweblogger/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
